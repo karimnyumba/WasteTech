2288a991138d1797d83a2cd216f8a6e4cc707461
DEOGASS WIDAMBE <87471672+widambeDeograss@users.noreply.github.com>
2023-06-24 12:57:48 +0300

    map priview

--- waste-tech-ui/src/App.js
+++ waste-tech-ui/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import Intro from './components/Dash/Intro'
+import ContactSection from './components/contact-section/ContactSection'
+import Disclaimer from './components/Disclaimer/Disclaimer'
+import Footer from './components/Footer/Footer'
+import './App.css'
+import Map from './components/Map/MapPreview'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Intro />
+      <ContactSection />
+      <Map />
+      <Disclaimer />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
