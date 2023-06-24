c05218047050f1a6074f8b03b604216fe0359bad
Rasta Bujo <buyegiminzi@gmail.com>
2023-06-24 16:09:35 +0300

    Link to model

--- waste-tech-ui/src/components/contact-section/Form.jsx
+++ waste-tech-ui/src/components/contact-section/Form.jsx
@@ -24,13 +24,17 @@ const formInputs = [
 const Form = () => (
   <form className="form">
     <h2 className="form-h2">Bin Locations</h2>
-   {/* // Lists goes here */}
-   <ol>
-    <li>National Examination Council</li>
-    <li>College of Information and Communication Technology CoICT</li>
-    <li>Institute of Social Work</li>
-   </ol>
-
+    {/* // Lists goes here */}
+    <ol>
+      <li>National Examination Council</li>
+      <li>College of Information and Communication Technology CoICT</li>
+      <li>Institute of Social Work</li>
+    </ol>
+    <a href='http://wastemanage.azurewebsites.net/'>
+      <h3>
+        The AI Model For Waste Classification
+      </h3>
+    </a>
     {/* <Icon className="form-submit" icon={sendCircle} /> */}
 
     {/* <button className="form-submit" type="submit">
