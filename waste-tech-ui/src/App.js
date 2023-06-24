ca251cf2770ed7b3db47f2f920f547defe81a72f
DEOGASS WIDAMBE <87471672+widambeDeograss@users.noreply.github.com>
2023-06-24 13:46:35 +0300

    Update App.js

--- waste-tech-ui/src/App.js
+++ waste-tech-ui/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Intro from './components/Dash/Intro'
 import ContactSection from './components/contact-section/ContactSection'
 import Disclaimer from './components/Disclaimer/Disclaimer'
-import Footer from './components/Footer/Footer'
+import Footer from './components/footer/Footer'
 import './App.css'
 import Map from './components/Map/MapPreview'
 
